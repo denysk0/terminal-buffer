a885fbfd3ee095ac66e6524848e2fce001cea9bc
denysk0 <denys.kyrychenko0@gmail.com>
2026-03-05 17:04:55 +0100

    test: define current attributes behavior

---
+++ src/test/java/com/example/terminalbuffer/CurrentAttributesTest.java
@@ -0,0 +1,29 @@
+package com.example.terminalbuffer;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CurrentAttributesTest {
+
+    @Test
+    void defaultCurrentAttributesIsDefault() {
+        var buf = new TerminalBuffer(80, 24, 0);
+        assertEquals(Attributes.DEFAULT, buf.getCurrentAttributes());
+    }
+
+    @Test
+    void setCurrentAttributesUpdatesState() {
+        var buf = new TerminalBuffer(80, 24, 0);
+        var attrs = new Attributes(TerminalColor.RED, TerminalColor.BLUE, Style.BOLD.mask());
+        buf.setCurrentAttributes(attrs);
+        assertEquals(attrs, buf.getCurrentAttributes());
+    }
+
+    @Test
+    void setCurrentAttributesNullFallsBackToDefault() {
+        var buf = new TerminalBuffer(80, 24, 0);
+        buf.setCurrentAttributes(null);
+        assertEquals(Attributes.DEFAULT, buf.getCurrentAttributes());
+    }
+}
